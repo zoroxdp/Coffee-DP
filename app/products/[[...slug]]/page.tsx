99b4b30cabae8bb32cd2b0271628be998d8c1ed7
Deependra Singh Shekhawat <zoroxdp@gmail.com>
2024-08-20 00:09:16 +0530

    feat: Product Page added and more

---
+++ app/products/[[...slug]]/page.tsx
@@ -0,0 +1,37 @@
+import {
+  getProducts,
+  listCategories, sortMap,
+  getProductBySlugOrId
+} from '@/lib/swell/products'
+
+import Products from '@/components/products/Products'
+import Product from '@/app/components/products/Product'
+
+const Page = async ({
+  params, searchParams
+}: {
+  params: { slug?: string[] }
+  searchParams: { sort?: string }
+}) => {
+  const { slug } = params;
+  const productSlug = slug?.length === 1 && slug[0];
+  const categorySlug = slug?.length === 2 && slug[1];
+
+  if (productSlug) {
+    const product = await getProductBySlugOrId(productSlug);
+    return <Product product={product} />
+  }
+
+  const { sort } = searchParams;
+  const mappedSort = sort ? sortMap.get(sort) : '';
+  const { results: products } = await getProducts({
+    sort: mappedSort,
+    ...(categorySlug ? { categories: [categorySlug] } : {})
+  })
+
+  const { results: categories } = await listCategories();
+
+  return <Products products={products} categories={categories} />
+}
+
+export default Page
